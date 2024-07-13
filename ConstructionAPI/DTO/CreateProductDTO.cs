350c0eb5ae17e8f3b60a57d5329c012e7d0b3a63
IlkinNovruzov <inovruzov2004@gmail.com>
2024-07-13 12:13:26 +0400

    ilkin update

--- ConstructionAPI/DTO/CreateProductDTO.cs
+++ ConstructionAPI/DTO/CreateProductDTO.cs
@@ -1,6 +1,17 @@
-﻿namespace ConstructionAPI.DTO
+﻿using ConstructionAPI.Models;
+
+namespace ConstructionAPI.DTO
 {
     public class CreateProductDTO
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsVIP { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public int? ShopId { get; set; }
+        public List<IFormFile> ImageFiles { get; set; }
+        //il
     }
 }
